2e5e73eb2e080ae7fc424af5df6ee5d2bd137165
haazuk <hazikali3@gmail.com>
2023-07-25 21:45:56 +1200

    Changed the header
    Removed default header and added a new navigation bar. Needs to be centered.

--- wp-content/themes/story_ante/header.php
+++ wp-content/themes/story_ante/header.php
@@ -12,10 +12,14 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+    
+ 
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/header.css' ?>">
+    
 
 	<?php wp_head(); ?>
 </head>
@@ -26,34 +30,25 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'story_ante' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$story_ante_description = get_bloginfo( 'description', 'display' );
-			if ( $story_ante_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $story_ante_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
+        
+        
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'story_ante' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
+            <ul> 
+                <li> 
+                   <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+    <li>
+      <a href="#">Services</a>
+    </li>
+    <li>
+      <a href="#">Contact</a>
+    </li>
+  </ul>
+    
+		
+  
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
